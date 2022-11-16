9e8398080078a94c7bd9b6eebac47cdd6f7e89c3
willi <liw1115991@gmail.com>
2022-11-17 00:13:42 +0800

    栏目区域布局

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,9 +1,5 @@
-// pages/category/category.js
+// pages/login/login.js
 Page({
-
-  getFruitsByCategory(){
-    console.log('获取指定类别的水果');
-  },
 
   /**
    * 页面的初始数据
@@ -15,56 +11,56 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage() {
 
   }
 })
